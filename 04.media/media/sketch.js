b881703c36d55a726aa9e09d30dca0beba977adf
Amarajan7 <amarajan134340@gmail.com>
2025-10-27 11:46:10 +0000

    Add files via upload

---
+++ 04.media/media/sketch.js
@@ -0,0 +1,21 @@
+let hum;
+let img;
+let capture;
+let video;
+
+// Declare variable 'img'.
+
+function preload() {
+  img = loadImage('Antenna.jpg'); // Load image
+}
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+}
+
+function draw() {
+  // Display image at actual size at x/y (0,0)
+  image(img, 0, 0);
+  // Displays half size image at x/y (0, 100)
+  //image(img, 0, 100, img.width/2, img.height/2);
+}
